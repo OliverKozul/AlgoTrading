70ea2c6a240d7ed5473aa48844850ff6eb315c08
OliverK666 <oliverkozul@gmail.com>
2024-10-14 14:04:54 +0200

    Added requirements.txt

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import dataManipulator as dm
 
 
 if __name__ == "__main__":
-    symbols = dm.loadSymbols('R2000')
+    symbols = dm.loadSymbols('SP')
     # symbols = ['AMD', 'NVDA', 'CAT']
     # symbols = ['TER']
 
-    strategy = 'rocTrendFollowingBear'
+    strategy = 'rocTrendFollowingBull'
 
     st.runMasterBacktest(symbols, strategy)
         
